b84880c00e310d280987d4ba1405f25b1278a941
Theodore Wolff <work@theo.gg>
2025-09-27 19:07:55 +0300

    minor comment syntax update

--- functions.php
+++ functions.php
@@ -3,13 +3,13 @@
         define('TWWP_CHILD_DIST', get_stylesheet_directory_uri() . '/dist');
     }
 
-    // Wire extras - core files
+    // Wire extras: core files
     require_once __DIR__ . '/extras/helpers.php';
     require_once __DIR__ . '/extras/setup.php';
     require_once __DIR__ . '/extras/theme-functions.php';
     require_once __DIR__ . '/extras/ajax.php';
 
-    // Wire extras - custom files
+    // Wire extras: custom files
     require_once __DIR__ . '/extras/post-types/post-types.php';
     require_once __DIR__ . '/extras/shortcodes/shortcodes.php';
     require_once __DIR__ . '/extras/integrations/integrations.php';
